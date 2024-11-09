f61d6e51856aa9a87af23cb3a8e2f05e811b51f4
이민석 <leem980425@gmail.com>
2024-11-09 20:03:49 +0900

    [Fix]Base Char,Mob Prefab
    -몬스터, 플레이어 프리팹 대폭 수정

---
+++ Assets/Scripts/Controller/InputController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+   
+    public void OnMouseClicked()
+    {
+
+    }
+
+    public void OnMouseMoved()
+    {
+
+    }
+
+}
